97b92ce6e65cf4f501a62d039f297397d4a7758b
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2014-11-21 19:03:21 +0100

    Enabled Pretty URLs

--- config/web.php
+++ config/web.php
@@ -38,6 +38,13 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
+        'urlManager' => [
+            'class'=>'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            /*'rules' => [
+            ],*/
+        ],
     ],
     'params' => $params,
 ];
@@ -45,7 +52,10 @@ $config = [
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+    $config['modules']['debug'] = [
+        'class'=>'yii\debug\Module',
+        'allowedIPs'=>['*.*.*.*']
+    ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = 'yii\gii\Module';
