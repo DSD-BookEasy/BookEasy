be73807ead9eb60e6bfb21f0175c7641149d6823
LeoBlackSoul <ste-camp@users.noreply.github.com>
2014-11-22 17:42:31 +0100

    Fixed Email Migration

--- migrations/m141122_131841_change_mail.php
+++ migrations/m141122_131841_change_mail.php
@@ -7,26 +7,34 @@ class m141122_131841_change_mail extends Migration
 {
     public function up()
     {
-		//change attribute name from e-mail to email
-		//from booking
-		$this->renameColumn(
-							"booking",
-							"e-mail",
-							"email"
-		);
-		//from booking
-		$this->renameColumn(
-							"staff",
-							"e-mail",
-							"email"
-		);
-
+	    //change attribute name from e-mail to email
+	    //from booking
+	    $this->renameColumn(
+    	    "Booking",
+    	    "e-mail",
+    	    "email"
+    	);
+    	//from booking
+    	$this->renameColumn(
+    	    "Staff",
+    	    "e-mail",
+    	    "email"
+	    );
     }
 
     public function down()
     {
-        echo "m141122_131841_change_mail cannot be reverted.\n";
-
-        return false;
+        $this->renameColumn(
+	        "Booking",
+	        "email",
+	        "e-mail"
+	    );
+	
+	//from booking
+	    $this->renameColumn(
+	        "Staff",
+	        "email",
+	        "e-mail"
+	    );
     }
 }
