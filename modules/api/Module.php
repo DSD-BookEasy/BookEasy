e7f2efb2ac3d1bf4048c0529743561b6a4a9a133
Paolo Manca <paolo@manca.eu>
2014-11-26 11:26:48 +0100

    Create basic modules structure

---
+++ modules/api/Module.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\modules\api;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\api\controllers';
+    public function init()
+    {
+        parent::init();
+    }
+}
---
+++ modules/api/modules/v1/Module.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\modules\api\modules\v1;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\api\modules\v1\controllers';
+    public function init()
+    {
+        parent::init();
+    }
+}
