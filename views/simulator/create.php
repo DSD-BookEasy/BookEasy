90b97d77c9a8b8bed56b75040fe46718e90741f0
Marco Edemanti <edemantimarco@gmail.com>
2014-11-27 16:38:10 +0100

    add simulator mvc

---
+++ views/simulator/create.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\simulator */
+
+$this->title = Yii::t('app', 'Add new {modelClass}', [
+    'modelClass' => 'Simulator',
+]);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Simulators'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="simulator-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
