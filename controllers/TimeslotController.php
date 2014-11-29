209a61f416b435587344a52b7b1149c92698a701
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2014-11-29 22:06:34 +0100

    Deleted useless methods in timeslotcontroller

--- controllers/TimeslotController.php
+++ controllers/TimeslotController.php
@@ -37,104 +37,26 @@ class TimeslotController extends Controller
      */
     public function actionIndex()
     {
-        $sim=\Yii::$app->request->get("simulator");
-        if($sim==null || ((int)$sim)==0){
+        $sim = \Yii::$app->request->get("simulator");
+        if ($sim == null || ((int)$sim) == 0) {
             throw new ErrorException();
         }
-        $show=\Yii::$app->request->get("week",date('c'));
-        if(strtotime($show)==false){
-            $show=date('c');
+        $show = \Yii::$app->request->get("week", date('c'));
+        if (strtotime($show) == false) {
+            $show = date('c');
         }
 
         //Find timeslots in the week of the specified day
-        $borders=$this->findWeekFromDay($show);
-        $thisWeek=Timeslot::find()->
-            where(['id_simulator' => $sim])->
-            andWhere(['>=','start',strftime("%Y-%m-%d",$borders[0])])->
-            andWhere(['<=','end',strftime("%Y-%m-%d",$borders[1])])->all();
+        $borders = $this->findWeekFromDay($show);
+        $thisWeek = Timeslot::find()->
+        where(['id_simulator' => $sim])->
+        andWhere(['>=', 'start', strftime("%Y-%m-%d", $borders[0])])->
+        andWhere(['<=', 'end', strftime("%Y-%m-%d", $borders[1])])->all();
 
         return $this->render('index', [
-            'week'=>$show,
-            'slots'=>$thisWeek
+            'week' => $show,
+            'slots' => $thisWeek
         ]);
-    }
-
-    /**
-     * Displays a single Timeslot model.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionView($id)
-    {
-        return $this->render('view', [
-            'model' => $this->findModel($id),
-        ]);
-    }
-
-    /**
-     * Creates a new Timeslot model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new Timeslot();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('create', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Updates an existing Timeslot model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        } else {
-            return $this->render('update', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Deletes an existing Timeslot model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-    /**
-     * Finds the Timeslot model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Timeslot the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Timeslot::findOne($id)) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
     }
 
     /**
