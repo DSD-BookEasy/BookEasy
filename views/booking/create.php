d6f302df5b212b0bb332b91f70e5617cbe266046
ske14002 <Sebastian@c-139-25-vas-l3.cust.mdfnet.se>
2014-12-03 14:39:35 +0100

    Showing booking information in create booking view

--- views/booking/create.php
+++ views/booking/create.php
@@ -16,8 +16,31 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
+    <p>You are about to book the following simulator:</p>
+
+    <?php
+
+    $price_simulator = 0;
+    if (empty($timeslots) == false) {
+        foreach ($timeslots as $timeslot) {
+            $price_simulator += isset($timeslot->cost) ? $timeslot->cost : 0;
+        }
+    }
+
+    ?>
+
+    <ul>
+        <li>Start: <?= $timeslots[0]->start ?></li>
+        <li>End: <?= $timeslots[0]->end ?></li>
+        <li>Cost: <?= $price_simulator ?> SEK</li>
+        <li>Entrance: 80 SEK</li>
+    </ul>
+
+    <p>Please provide the following information to continue.</p>
+
     <?= $this->render('_form', [
         'model' => $model,
+        'timeslots' => $timeslots,
     ]) ?>
 
 </div>
