7b8559663e01230eb1a9037085afbe1bd4b232df
Mert Ergun <mert.rgun@gmail.com>
2014-12-10 12:55:10 +0100

    To provide an example test to check whether acceptance tests are working or not

--- tests/codeception/acceptance/HomeCept.php
+++ tests/codeception/acceptance/HomeCept.php
@@ -3,7 +3,8 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that home page works');
 $I->amOnPage(Yii::$app->homeUrl);
-$I->see('My Company');
-$I->seeLink('About');
-$I->click('About');
-$I->see('This is the About page.');
+$I->see('Welcome');
+$I->see('Choose the simulator you wish to book');
+$I->seeLink('Login');
+$I->click('Login');
+$I->see('Staff Login');
