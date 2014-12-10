79877059ace6b7f6fd672e5f9746441e425d91c6
BaggioAlessandro <alebaggio91@hotmail.it>
2014-12-10 10:15:41 +0100

    unbinding of model on update timeslot

--- controllers/TimeSlotController.php
+++ controllers/TimeSlotController.php
@@ -84,8 +84,11 @@ class TimeSlotController extends Controller
     {
         $model = $this->findModel($id);
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+        if ($model->load(Yii::$app->request->post())){
+            $model->id_timeSlotModel = NULL;
+            if($model->save()){
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
         } else {
             return $this->render('update', [
                 'model' => $model,
