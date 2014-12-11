d10b319608ec2c7d7e776e83b15cd73fdab7b508
Paolo Manca <paolo@manca.eu>
2014-12-11 18:16:46 +0100

    Revert "update"
    This reverts commit 1cbd02023107666b433cb60bd5031b6e2075a06e.

---
+++ config/db.example.php
@@ -0,0 +1,10 @@
+<?php
+//Make your own db.php file in this folder to configure your Database connection
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
