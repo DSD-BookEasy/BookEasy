53d52f01dbe7d99d7d6892e5960af46c07114234
BaggioAlessandro <alebaggio91@hotmail.it>
2014-12-12 17:41:37 +0100

    add down to last migration

--- migrations/m141212_162703_change_name_to_status_slot.php
+++ migrations/m141212_162703_change_name_to_status_slot.php
@@ -8,12 +8,11 @@ class m141212_162703_change_name_to_status_slot extends Migration
     public function up()
     {
         $this->renameColumn('TimeSlot', 'status', 'creation_mode');
-    }
+}
 
     public function down()
     {
-        echo "m141212_162703_change_name_to_status_slot cannot be reverted.\n";
-
+        $this->renameColumn('TimeSlot', 'creation_mode', 'status');
         return false;
     }
 }
