2c63cd9ba8487a1191284712bc83c85837d16d98
Paolo Manca <paolo@manca.eu>
2014-12-15 16:41:47 +0100

    Added different index for logged in users

---
+++ views/site/admin-index.php
@@ -0,0 +1,43 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $simulators app\models\Simulator[]*/
+
+use yii\helpers\Url;
+
+$this->title = 'Västerås Flygmuseum';
+?>
+<div class="site-index">
+
+    <div class="body-content">
+        <div class="jumbotron">
+            <h1><?= Yii::t('app', 'Admin control panel') ?></h1>
+
+            <p class="lead"><?= Yii::t('app', 'Choose what you wish to manage'); ?></p>
+            <br>
+            <p>
+                <a class="btn btn-default btn-lg"
+                   href="<?= Url::to([
+                       'simulator/index',
+                   ]); ?>"><?= Yii::t('app',
+                        'Simulators'); ?></a>
+                <a class="btn btn-default btn-lg"
+                   href="<?= Url::to([
+                       'booking/index',
+                   ]); ?>"><?= Yii::t('app',
+                        'Bookings'); ?></a>
+                <a class="btn btn-default btn-lg"
+                   href="<?= Url::to([
+                       'timeslot/index',
+                   ]); ?>"><?= Yii::t('app',
+                        'Timeslots'); ?></a>
+                <a class="btn btn-default btn-lg"
+                   href="<?= Url::to([
+                       'timeslot-model/index',
+                   ]); ?>"><?= Yii::t('app',
+                        'Timeslot Models'); ?></a>
+            </p>
+
+        </div>
+
+    </div>
+</div>
