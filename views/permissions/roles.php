d0ec37b0ace793a2dc61791561972a765f384c1a
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2014-12-17 15:20:09 +0100

    Changing console config too, in order to be able to apply RBAC migration.

---
+++ views/permissions/roles.php
@@ -0,0 +1,6 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h1><?=\Yii::t('app',"Administrative Roles") ?></h1>
+<h3><?=\Yii::t('app',"From here you can manage the user roles available in the system")?></h3>
+
