6f78caece51bd17c67064fe99b5abd6a3b586bbc
BaggioAlessandro <alebaggio91@hotmail.it>
2014-12-17 16:38:26 +0100

    miss a piece

--- models/Booking.php
+++ models/Booking.php
@@ -44,7 +44,7 @@ class Booking extends \yii\db\ActiveRecord
             ['email', 'email'],
             [['name', 'surname'], 'required'],
             ['email', 'required', 'on' => ['weekdays']],
-            [[name', 'surname', 'token'], 'required', 'on' => ['search']]
+            [['name', 'surname', 'token'], 'required', 'on' => ['search']]
 
         ];
     }
