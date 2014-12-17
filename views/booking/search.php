d1b75101821933b25a72ffd42c8035430a24bd01
BaggioAlessandro <alebaggio91@hotmail.it>
2014-12-17 16:14:46 +0100

    id request anymore for booking search

--- views/booking/search.php
+++ views/booking/search.php
@@ -12,8 +12,6 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id')->textInput() ?>
-
     <?= $form->field($model, 'name')->textInput() ?>
 
     <?= $form->field($model, 'surname')->textInput() ?>
