a24858f950bf5997c843718dda9666b77615cf23
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2014-12-18 20:04:30 +0100

    Implemented edit action for a user with roles for permissions management

--- controllers/StaffController.php
+++ controllers/StaffController.php
@@ -2,8 +2,10 @@
 
 namespace app\controllers;
 
+use Yii;
 use app\models\Staff;
 use yii\filters\AccessControl;
+use yii\web\NotFoundHttpException;
 
 class StaffController extends \yii\web\Controller
 {
@@ -31,10 +33,10 @@ class StaffController extends \yii\web\Controller
     public function actionLogin()
     {
         //Already loggedin users should not access this page
-        if (!\Yii::$app->user->isGuest) {
+        if (!Yii::$app->user->isGuest) {
             return $this->goHome();
         }
-        $loginData=\Yii::$app->request->post('Staff');
+        $loginData=Yii::$app->request->post('Staff');
         //No data sent, show the form, link the controller with the view
         if(empty($loginData)){
             return $this->render('login', [
@@ -44,7 +46,7 @@ class StaffController extends \yii\web\Controller
         else{
             $staff=Staff::findOne(['user_name'=>$loginData['user_name']]);
             if(!empty($staff) and $staff->isValidPassword($loginData['password'])){
-                \Yii::$app->user->login($staff, 3600*24*30);
+                Yii::$app->user->login($staff, 3600*24*30);
                 return $this->goHome();
             }
             else{
@@ -52,7 +54,7 @@ class StaffController extends \yii\web\Controller
                 $staff->user_name=$loginData['user_name'];
                 return $this->render('login', [
                     'model' => $staff,
-                    'error' => \Yii::t('app','Invalid Username or Password')
+                    'error' => Yii::t('app','Invalid Username or Password')
                 ]);
             }
         }
@@ -66,5 +68,47 @@ class StaffController extends \yii\web\Controller
     {
         \Yii::$app->user->logout();
         return $this->goBack();
+    }
+
+    public function actionEdit($id){
+        $s=Staff::findOne((int)$id);
+        if(empty($s)){
+            throw new NotFoundHttpException(Yii::t('app',"The specified user doesn't exist"));
+        }
+
+        if(Yii::$app->request->getIsPost()) {
+            $s->load(Yii::$app->request->post());
+            if($s->save()){//If basic save is successfull, go on with permissions save
+                $this->updateRoles($s,Yii::$app->request->post('roles',[]));
+            }
+        }
+
+        return $this->render('edit',[
+            'user' => $s,
+            'allRoles' => Yii::$app->authManager->getRoles(),
+            'roles' => Yii::$app->authManager->getRolesByUser($s->id)
+        ]);
+    }
+
+    /**
+     * Updates the roles assigned to a user basing on the input from the POST
+     * @param Staff $user the user to update
+     * @param array $roles an array of roles to add. It should be indexed with the names of the roles
+     */
+    private function updateRoles($user,$roles)
+    {
+        $oldRoles = Yii::$app->authManager->getRolesByUser($user->id);
+
+        $toDelete = array_diff_key($oldRoles,$roles);
+        $toAdd = array_diff_key($roles, $oldRoles);
+
+        foreach($toDelete as $roleName => $rObj){
+            Yii::$app->authManager->revoke($rObj,$user->id);
+        }
+
+        foreach($toAdd as $roleName => $value){
+            $r = Yii::$app->authManager->getRole($roleName);
+            Yii::$app->authManager->assign($r,$user->id);
+        }
     }
 }
