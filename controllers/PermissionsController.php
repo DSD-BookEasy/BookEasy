4d09e836134add5a536a3f2e8cd3b566982d4f4c
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2014-12-18 02:01:50 +0100

    Reformatted PermissionsController to match standard

--- controllers/PermissionsController.php
+++ controllers/PermissionsController.php
@@ -14,7 +14,7 @@ class PermissionsController extends \yii\web\Controller
      */
     public function actionRoles()
     {
-        return $this->render('roles',[
+        return $this->render('roles', [
             'roles' => Yii::$app->authManager->getRoles()
         ]);
     }
@@ -23,65 +23,67 @@ class PermissionsController extends \yii\web\Controller
      * Adds a new role in the system
      * @return string
      */
-    public function actionAddRole(){
-        $post=Yii::$app->request->post('AdminRole');
-        if(!empty($post) and !empty($post['name'])){
-            $new_r=Yii::$app->authManager->createRole($post['name']);
-            if(!empty($post['description'])) {
-                $new_r->description=$post['description'];
+    public function actionAddRole()
+    {
+        $post = Yii::$app->request->post('AdminRole');
+        if (!empty($post) and !empty($post['name'])) {
+            $new_r = Yii::$app->authManager->createRole($post['name']);
+            if (!empty($post['description'])) {
+                $new_r->description = $post['description'];
             }
             Yii::$app->authManager->add($new_r);
             $this->redirect(['permissions/roles']);
         }
 
-        return $this->render('add-role',[
+        return $this->render('add-role', [
             'role' => new AdminRole(Yii::$app->authManager->createRole(''))
         ]);
     }
 
     /**
      * Updates an existing role
-     * @param $name: the name of the role to update
+     * @param $name : the name of the role to update
      */
-    public function actionUpdateRole($name){
-        $r=Yii::$app->authManager->getRole($name);
-        if(empty($r)){
+    public function actionUpdateRole($name)
+    {
+        $r = Yii::$app->authManager->getRole($name);
+        if (empty($r)) {
             throw new NotFoundHttpException("The specified role could not be found");
         }
 
-        $post=Yii::$app->request->post('AdminRole');
-        if(!empty($post) and !empty($post['name'])){
-            $r->name=$post['name'];
-            if(!empty($post['description'])) {
-                $r->description=$post['description'];
+        $post = Yii::$app->request->post('AdminRole');
+        if (!empty($post) and !empty($post['name'])) {
+            $r->name = $post['name'];
+            if (!empty($post['description'])) {
+                $r->description = $post['description'];
             }
-            Yii::$app->authManager->update($name,$r);
+            Yii::$app->authManager->update($name, $r);
             $this->redirect(['permissions/roles']);
         }
 
-        return $this->render('add-role',[
+        return $this->render('add-role', [
             'role' => new AdminRole($r)
         ]);
     }
 
     /**
      * Deletes an existing role
-     * @param $name: the name of the role to delete
+     * @param $name : the name of the role to delete
      */
-    public function actionDeleteRole($name){
-        $r=Yii::$app->authManager->getRole($name);
-        if(empty($r)){
+    public function actionDeleteRole($name)
+    {
+        $r = Yii::$app->authManager->getRole($name);
+        if (empty($r)) {
             throw new NotFoundHttpException("The specified role could not be found");
         }
 
-        $post=Yii::$app->request->post();
+        $post = Yii::$app->request->post();
 
-        if(empty($post)) {
+        if (empty($post)) {
             echo $this->render('delete-role', [
                 'role' => new AdminRole($r)
             ]);
-        }
-        else{
+        } else {
             Yii::$app->authManager->remove($r);
             $this->redirect('roles');
         }
@@ -93,25 +95,26 @@ class PermissionsController extends \yii\web\Controller
      */
     public function actionIndex()
     {
-        $post=Yii::$app->request->post();
-        if(!empty($post)){
+        $post = Yii::$app->request->post();
+        if (!empty($post)) {
             $this->updateAssignments($post);
         }
 
-        $roles=Yii::$app->authManager->getRoles();
-        $assignments=[];
-        foreach($roles as $r){
-            $assignments[$r->name]=Yii::$app->authManager->getPermissionsByRole($r->name);
+        $roles = Yii::$app->authManager->getRoles();
+        $assignments = [];
+        foreach ($roles as $r) {
+            $assignments[$r->name] = Yii::$app->authManager->getPermissionsByRole($r->name);
         }
 
-        return $this->render('index',[
+        return $this->render('index', [
             'roles' => $roles,
             'permissions' => Yii::$app->authManager->getPermissions(),
             'assignments' => $assignments
         ]);
     }
 
-    private function updateAssignments($post){
+    private function updateAssignments($post)
+    {
 
     }
 }
