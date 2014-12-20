1c445a6d317cdc67feba0cf45e04dfa027251fc1
Paolo Manca <paolo@manca.eu>
2014-12-20 19:13:47 +0100

    yii2images general setup

--- config/web.php
+++ config/web.php
@@ -8,8 +8,22 @@ $config = [
     //'language' => 'sv-SE',
     'bootstrap' => ['log'],
     'modules' => [
+
         'api' => [
             'class' => 'app\modules\api\Module',
+        ],
+        'yii2images' => [
+            'class' => 'rico\yii2images\Module',
+            //be sure, that permissions ok
+            //if you cant avoid permission errors you have to create "images" folder in web root manually and set 777 permissions
+            'imagesStorePath' => 'images/store',
+            //path to origin images
+            'imagesCachePath' => 'images/cache',
+            //path to resized copies
+            'graphicsLibrary' => 'GD',
+            //but really its better to use 'Imagick'
+            'placeHolderPath' => '@webroot/images/placeHolder.png',
+            // if you want to get placeholder when image not exists, string will be processed by Yii::getAlias
         ],
     ],
     'components' => [
