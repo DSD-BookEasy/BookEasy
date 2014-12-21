fba0347ed893385d2978ea29e8095dbe56c0a3ab
Paolo Manca <paolo@manca.eu>
2014-12-21 16:59:36 +0100

    Changed the folder for uploads and added it to ignore

--- config/web.php
+++ config/web.php
@@ -15,15 +15,13 @@ $config = [
         'yii2images' => [
             'class' => 'rico\yii2images\Module',
             //be sure, that permissions ok
-            //if you cant avoid permission errors you have to create "images" folder in web root manually and set 777 permissions
-            'imagesStorePath' => 'images/store',
+            //if you cant avoid permission errors you have to create "uploads" folder in web root manually and set 777 permissions
+            'imagesStorePath' => 'uploads/images/store',
             //path to origin images
-            'imagesCachePath' => 'images/cache',
+            'imagesCachePath' => 'uploads/images/cache',
             //path to resized copies
             'graphicsLibrary' => 'GD',
             //but really its better to use 'Imagick'
-            'placeHolderPath' => '@webroot/images/placeHolder.png',
-            // if you want to get placeholder when image not exists, string will be processed by Yii::getAlias
         ],
     ],
     'components' => [
