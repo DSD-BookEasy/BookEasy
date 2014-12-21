4044143c3e93301a90f5404ab5e6923fd22d32fb
Paolo Manca <paolo@manca.eu>
2014-12-21 17:00:41 +0100

    [5] Set up yii2images in the model

--- models/Simulator.php
+++ models/Simulator.php
@@ -4,6 +4,9 @@ namespace app\models;
 
 use Yii;
 use DateTime;
+use yii\db\ActiveRecord;
+use yii\web\UploadedFile;
+
 /**
  * This is the model class for table "Simulator".
  *
@@ -12,17 +15,25 @@ use DateTime;
  * @property string $description
  * @property integer $flight_duration
  * @property integer $price_simulation
-* @property DateTime $date
-
-
-
+ * @property UploadedFile $uploadFile
+ *
+ * The following methods are provided by the behavior rico\yii2images\behaviors\ImageBehave
+ * @method attachImage($absolutePath, $isMain = false)
+ * @method setMainImage($img)
+ * @method clearImagesCache()
+ * @method getImages()
+ * @method getImage()
+ * @method removeImages()
+ * @method removeImage(Image $img)
+ * @see https://github.com/CostaRico/yii2-images
  */
-class Simulator extends \yii\db\ActiveRecord
+class Simulator extends ActiveRecord
 {
+    public $uploadFile;
+
     /**
      * @inheritdoc
      */
-
     public static function tableName()
     {
         return 'Simulator';
@@ -31,13 +42,25 @@ class Simulator extends \yii\db\ActiveRecord
     /**
      * @inheritdoc
      */
-
     public function rules()
     {
         return [
             [['description'], 'string'],
             [['flight_duration', 'price_simulation'], 'integer'],
             [['name'], 'string', 'max' => 255],
+            [['uploadFile'], 'image']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'image' => [
+                'class' => 'rico\yii2images\behaviors\ImageBehave',
+            ]
         ];
     }
 
