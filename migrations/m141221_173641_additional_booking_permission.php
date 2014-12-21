ae9f0c91c177fdbbc3e306cdf6553f8f7eefca08
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2014-12-21 18:54:00 +0100

    Wrong code in last migration

--- migrations/m141221_173641_additional_booking_permission.php
+++ migrations/m141221_173641_additional_booking_permission.php
@@ -7,14 +7,14 @@ class m141221_173641_additional_booking_permission extends Migration
 {
     public function up()
     {
-        $p=Yii::$app->authManager->createPermission('ManageBookings');
+        $p=Yii::$app->authManager->createPermission('manageBookings');
         $p->description="view and edit all the existing bookings";
         Yii::$app->authManager->add($p);
     }
 
     public function down()
     {
-        $p=Yii::$app->authManager->getPermission("viewAllBookings");
+        $p=Yii::$app->authManager->getPermission("manageBookings");
         Yii::$app->authManager->remove($p);
     }
 }
