5e70224d514824ad9eb57dd3d8f014c355d9a917
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2014-12-23 15:26:00 +0100

    Added missing class in Parameter controller

--- controllers/ParameterController.php
+++ controllers/ParameterController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use app\models\Parameter;
 use yii\data\ActiveDataProvider;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
