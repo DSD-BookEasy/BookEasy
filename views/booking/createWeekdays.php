b30e4e88a78d0238545a926a67df0d4a6947de4c
Sebastian <ske14002>
2014-12-23 10:02:19 +0100

    #48: Price calculation

--- views/booking/createWeekdays.php
+++ views/booking/createWeekdays.php
@@ -20,12 +20,39 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p><?= Yii::t('app','You are about to send a request for opening the museum for a special visit and a flight simulation. Below please find the information about your booking')?>:</p>
 
+    <?php
+    $simulatorFee = 0;
+
+    if (empty($timeslots) == false) {
+        $timeSlot = $timeslots[0];
+
+        $startDateInSeconds = strtotime($timeSlot->start);
+        $endDateInSeconds = strtotime($timeSlot->end);
+
+        // Booked time span in milliseconds
+        $timeSpanInMillis = $endDateInSeconds - $startDateInSeconds;
+
+        // Booked simulator
+        $bookedSimulator = $timeSlot->simulator;
+
+        // Price for a single time slot of a simulator
+        // NOTE: Simulator stores time slot length in minutes
+        $initialPricingTimeSpanInSeconds = $bookedSimulator->flight_duration * 60;
+
+        // Total number of booked time slots
+        $numberOfBookedTimeSlots = ceil($timeSpanInMillis / $initialPricingTimeSpanInSeconds);
+
+        // Final simulator price
+        $simulatorFee = $numberOfBookedTimeSlots * $bookedSimulator->price_simulation;
+    }
+    ?>
+
     <?= Html::ul([
         Yii::t('app','Start: {0, date, medium} {0, time, short}', strtotime($timeslots[0]->start)),
         Yii::t('app','End: {0, date, medium} {0, time, short}', strtotime($timeslots[0]->end)),
         Yii::t('app','Entrance: {0, number, currency}', $entry_fee),
-        Yii::t('app','Flight Simulation: {0, number, currency}', $timeslots[0]->simulator->price_simulation),
-        Yii::t('app','Total Cost: {0, number, currency}', $entry_fee + $timeslots[0]->simulator->price_simulation),
+        Yii::t('app','Flight Simulation: {0, number, currency}', $simulatorFee),
+        Yii::t('app','Total Cost: {0, number, currency}', $entry_fee + $simulatorFee),
         Yii::t('app','Additional costs may be applied for the museum opening out of usual opening hours. You will receive the final price as soon as the staff can confirm your booking.')
     ]);
     ?>
