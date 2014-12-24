e576ec5ac192692150ee6ce223e5de75bb5a9373
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2014-12-24 19:19:05 +0100

    [51] Cleaned simulator agenda from weekdays bookings code and added a link to the new future page

--- views/simulator/agenda.php
+++ views/simulator/agenda.php
@@ -61,31 +61,22 @@ $this->title = Yii::t('app', "{simulator}'s agenda", [
         </div>
 
     <div id="calendar_buttons">
-        <a href="<?= Url::to([
-                'agenda',
-                'id' => $simulator->id,
-                'week' => $prevWeek
-            ]) ?>"  class="btn btn-default">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                <?= \Yii::t('app', "Previous week"); ?>
-            </a>
+        <?php
+        echo Html::a(
+            Html::tag('span','',['class' => 'glyphicon glyphicon-chevron-left']),
+            ['agenda', 'id' => $simulator->id, 'week' => $prevWeek],
+            ['class' => 'btn btn-default']);
 
-        <a href="<?= Url::to([
-                'agenda',
-                'id' => $simulator->id,
-            ]) ?>" class="btn btn-default">
-                <?= \Yii::t('app', "Today"); ?>
-            </a>
+        echo Html::a(
+            Yii::t('app', "This Week"),
+            ['agenda', 'id' => $simulator->id],
+            ['class' => 'btn btn-default']);
 
-        <a href="<?= Url::to([
-                'agenda',
-                'id' => $simulator->id,
-                'week' => $nextWeek
-            ]) ?>" class="btn btn-default">
-                <?= \Yii::t('app', "Next week"); ?>
-                <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
-
+        echo Html::a(
+            Html::tag('span','',['class' => 'glyphicon glyphicon-chevron-right']),
+            ['agenda', 'id' => $simulator->id, 'week' => $nextWeek],
+            ['class' => 'btn btn-default']);
+        ?>
     </div>
 
     <div id="cal_datepicker" class="row">
@@ -136,7 +127,6 @@ $this->title = Yii::t('app', "{simulator}'s agenda", [
             }
 
             $bookUrl = Url::to(['/booking/create', 'timeslots[]' => '']);
-            $bookUrlWeekday = Url::to(['/booking/create-weekdays']);
 
             echo FullCalendar::widget([
                 'config' => [
@@ -154,20 +144,8 @@ $this->title = Yii::t('app', "{simulator}'s agenda", [
                     'defaultDate' => $currWeek->format("c"),
                     'events' => $events,
                     'eventRender' => new \yii\web\JsExpression('slotBooking'),
-                    //Features for booking during weekdays
-                    'selectable' => true,
-                    'selectOverlap' => new \yii\web\JsExpression("function(event)
-                {
-                    return (event.rendering === 'background' || event.rendering === 'inverse-background');
-                }"),
-                    'selectConstraint' => [
-                        'start' => $businessHours['start'],
-                        'end' => $businessHours['end'],
-                        'dow' => [0, 1, 2, 3, 4, 5, 6]
-                    ],
                     'minTime' => $borders['start'],
-                    'maxTime' => $borders['end'],
-                    'select' => new \yii\web\JsExpression("goToCreateWeekdays")
+                    'maxTime' => $borders['end']
                 ]
             ]); ?>
         </div>
@@ -194,6 +172,11 @@ $this->title = Yii::t('app', "{simulator}'s agenda", [
             ]);
             ?>
         </div>
+    </div>
+    <div class="alert alert-info">
+        <?= Html::tag('p',Yii::t('app',"It is also possible to make bookings out of the proposed timeslots above for special occasions. If you want to request a special booking please {click}",[
+            'click' => Html::a(Yii::t('app','click here'),['booking/create-weekdays'])
+        ]))?>
     </div>
 </div>
 
@@ -242,56 +225,6 @@ $this->title = Yii::t('app', "{simulator}'s agenda", [
                 element.tooltip();
             }
         }
-    }
-
-    function goToCreateWeekdays(start, end, jsEvent) {
-        var $d = $('#dialog');
-        if ($d.length == 0) {
-            $('body').append('<div id="dialog"></div>');
-            $d = $('#dialog');
-            $d.dialog({
-                modal: true,
-                autoOpen: false
-            });
-        }
-
-        // Restrict selection in the past
-        if (start < new Date()) {
-            // Set dialog's content message
-            $d.html('<?=\Yii::t('app',"Selected time span is in the past!") ?>');
-
-            // Set dialog's option buttons
-            $d.dialog("option", "buttons", {
-                "<?= \Yii::t('app',"Cancel")?>": function () {
-                    $('.fullcalendar').fullCalendar('unselect');
-                    $(this).dialog("close");
-                }
-            });
-        } else {
-            $d.html('<?=\Yii::t('app',"Do you want to send a request for making a special booking for this simulator?") ?><br />\
-            <?= \Yii::t('app',"Starting from")?>: <span class="new_timeslot">' + start.format("d-M-YYYY HH:mm") + '</span> <br />\
-            <?= \Yii::t('app',"Ending")?>: <span class="new_timeslot">' + end.format("d-M-YYYY HH:mm") + '</span>');
-            $d.dialog("option", "buttons", {
-                "<?=\Yii::t('app',"Confirm");?>": function () {
-                    window.location.href = '<?=$bookUrlWeekday?>?'
-                    + encodeURIComponent('timeslots[0][start]') + '=' + encodeURIComponent(start.format())
-                    + '&' + encodeURIComponent('timeslots[0][end]') + '=' + encodeURIComponent(end.format())
-                    + '&' + encodeURIComponent('timeslots[0][id_simulator]') + '=' + getSimulatorId();
-                },
-                "<?= \Yii::t('app',"Cancel")?>": function () {
-                    $('.fullcalendar').fullCalendar('unselect');
-                    $(this).dialog("close");
-                }
-            });
-        }
-
-        $d.dialog('open');
-    }
-
-    function getSimulatorId() {
-        var url = window.location.href;
-        var paths = url.split('/');
-        return paths[paths.length - 2];
     }
 </script>
 <?php
