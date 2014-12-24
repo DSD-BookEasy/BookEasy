c89d15c2b48673bfd705d7ba6e2e7c5082c12c3b
mertergun <mert.rgun@gmail.com>
2014-12-24 18:48:19 +0200

    Texts on the calendar view are now in bigger font

--- views/site/admin-index.php
+++ views/site/admin-index.php
@@ -19,42 +19,47 @@ $this->title = 'Västerås Flygmuseum';
                 <?php
                     $managements=[
                         [
-                            'url' => '/simulator/index',
+                            'url' => \yii\helpers\Url::to(['/simulator/index']),
                             'title' => 'Simulators',
                             'permission' => 'manageSimulator'
                         ],
                         [
-                            'url' => '/booking/index',
+                            'url' => \yii\helpers\Url::to(['/booking/index']),
                             'title' => 'Bookings',
                             'permission' => 'manageBookings'
                         ],
                         [
-                            'url' => '/timeslot/index',
+                            'url' => \yii\helpers\Url::to(['/timeslot/index']),
                             'title' => 'Timeslots',
                             'permission' => 'manageTimeslots'
                         ],
                         [
-                            'url' => '/timeslot-model/index',
+                            'url' => \yii\helpers\Url::to(['/timeslot-model/index']),
                             'title' => 'Timeslot Models',
                             'permission' => 'manageTimeslotModels'
                         ],
                         [
-                            'url' => '/staff/index',
+                            'url' => \yii\helpers\Url::to(['/staff/index']),
                             'title' => 'Staff',
                             'permission' => 'manageStaff'
                         ],
                         [
-                            'url' => '/permission/index',
+                            'url' => \yii\helpers\Url::to(['/staff/agenda']),
+                            'title' => 'Staff Agenda',
+                            'permission' => 'manageStaff'
+                        ],
+                        [
+                            'url' => \yii\helpers\Url::to(['/permission/index']),
                             'title' => 'Permissions Management',
                             'permission' => 'assignPermissions'
                         ],
                         [
-                            'url' => '/permission/roles',
+                            'url' => \yii\helpers\Url::to(['/permission/roles']),
                             'title' => 'Administrative Roles Management',
                             'permission' => 'manageRoles'
                         ],
                         [
-                            'url' => '/parameter/index',
+                            'url' => \yii\helpers\Url::to(['/parameter/index']),
                             'title' => 'System Parameters Management',
                             'permission' => 'manageParams'
                         ],
