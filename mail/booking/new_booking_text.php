e4789a2778f693716ffb0b14e68e93b0964cadec
BaggioAlessandro <alebaggio91@hotmail.it>
2014-12-28 17:08:25 +0100

    integration of dynamic text mail

--- mail/booking/new_booking_text.php
+++ mail/booking/new_booking_text.php
@@ -5,8 +5,9 @@ use yii\helpers\Url;
 /* @var $this \yii\web\View view component instance */
 /* @var $message \yii\mail\BaseMessage instance of newly created mail message */
 /* @var $id integer the id of the just-created booking */
-?>
-Hello,
-a new booking for the museum simulators has been received.
-Check it out here:
-<?= Url::to(['booking/view','id'=>$id],true); ?>
+
+$mail = Parameter::findOne($id);
+
+echo  $mail->value .
+    Url::to(['booking/view','id'=>$id],true)
+    ;
