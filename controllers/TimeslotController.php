015ad86c5671de6882037d0cae20681e25815c69
BaggioAlessandro <alebaggio91@hotmail.it>
2015-01-04 17:55:14 +0100

    fix bug in timeslot controller

--- controllers/TimeslotController.php
+++ controllers/TimeslotController.php
@@ -32,7 +32,7 @@ class TimeslotController extends Controller
             ],
             'access' => [
                 'class' => AccessControl::className(),
-                'except' => 'anon-calendar',
+                'except' => ['anon-calendar'],
                 'rules' => [
                     [
                         'allow' => true,
