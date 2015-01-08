8a9fee690a4897b065d19d1dd125109683c981cd
BaggioAlessandro <alebaggio91@hotmail.it>
2015-01-08 20:15:06 +0100

    html mail to customer

--- mail/booking/costumer_booking_html.php
+++ mail/booking/costumer_booking_html.php
@@ -4,4 +4,20 @@
  * User: Ale
  * Date: 06/01/2015
  * Time: 18:18
- */ 
+ */
+?>
+
+<?=
+    Html::tag('p', Yii::t('app', 'Here there are your booking information:')).
+    Html::tag('p', Yii::t('app', 'Your Secret key is {0}', $booking->token ))
+?>
+
+
+<?php
+    echo( Html::tag('p', Yii::t('app', 'You have booked the following simulator:')));
+
+    foreach($timeSlots as $slot){
+        echo( Html::tag('p', Yii::t('app', 'starting from {0}', $slot->start )));
+        echo( Html::tag('p', Yii::t('app', 'ending at {0}', $slot->end )));
+    }
+?>
