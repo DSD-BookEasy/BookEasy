a64e7a97d52de6661d6c137dc99a85a64169014f
BaggioAlessandro <alebaggio91@hotmail.it>
2015-01-08 20:39:12 +0100

    trying to make email better....

--- mail/booking/costumer_booking_html.php
+++ mail/booking/costumer_booking_html.php
@@ -9,22 +9,23 @@ use yii\helpers\Html;
  * Time: 18:18
  */
 ?>
-
+<div class="customer_booking_html">
 <?=
-    Html::tag('h2', Yii::t('app', 'Here there are your booking information:')).
-    Html::tag('h3', Yii::t('app', 'Your Secret key is {0}', $booking->token ))
+    Html::tag('h1', Yii::t('app', 'Here there are your booking information:'), ['class' => 'mail_bold']).
+    Html::tag('p', Yii::t('app', 'Your Secret key is {0}', $booking->token ), ['class' => 'mail_bold'])
 ?>
 
 
 <?php
-    echo( Html::tag('h3', Yii::t('app', 'You have booked the following simulator:')));
+    echo( Html::tag('p', Yii::t('app', 'You have booked the following simulator:'), ['class' => 'mail_bold']));
 
     foreach($timeSlots as $slot){
         $simulator = $slot->simulator;
-        echo(Html::tag('h3', Yii::t('app', '{0}', $simulator->name)));
+        echo(Html::tag('p', Yii::t('app', '{0}', $simulator->name), ['class' => 'mail_bold']));
         echo( Html::ul([
-            Yii::t('app', 'starting from {0}', $slot->start ).
+            Yii::t('app', 'starting from {0}', $slot->start ),
             Yii::t('app', 'ending at {0}', $slot->end )]
         ));
     }
 ?>
+</div>
