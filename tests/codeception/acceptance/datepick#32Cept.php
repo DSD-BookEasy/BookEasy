a737a7530418d0a15e1b61491c9e4212b5356dbd
Marco Edemanti <edemantimarco@gmail.com>
2015-01-09 17:27:24 +0100

    modified name of tes

---
+++ tests/codeception/acceptance/datepick#32Cept.php
@@ -0,0 +1,11 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo("datepick");
+$I->amOnPage("/index-test.php");
+$I->click("(//a[contains(text(),'Book »')])[2]");
+$I->wait(2);
+$I->fillField("#w1", "01/20/2015");
+$I->wait(2);
+$I->see("Jan 19 - 25, 2015", "h2");
+$I->wait(2);
+
