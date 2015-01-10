a0e470be981cd95ccd20d46adccfb2460e5fb0c3
Marco Edemanti <edemantimarco@gmail.com>
2015-01-10 13:54:31 +0100

    more fixes on the test

--- tests/codeception/acceptance/validateInfoBook#3Cept.php
+++ tests/codeception/acceptance/validateInfoBook#3Cept.php
@@ -6,7 +6,10 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo("validate input info of booking");
         $I->amOnPage("/index-test.php/");
         $I->click("(//a[contains(text(),'Book »')])[2]");
-$I->click("//div[@id='calendar_buttons']/a[3]");
+$I->wait(1);
+if(($I->grabTextFrom("h2"))=="Jan 5 - 11, 2015"){
+    $I->click("//div[@id='calendar_buttons']/a[3]");
+}
         $I->click("//div[@id='w0']/div[2]/div/table/tbody/tr/td/div/div/div[3]/table/tbody/tr/td[8]/div/a[6]/div");
 
 
