1dd71a9ed99616120f7472f2bffa219f49976054
Paolo Manca <paolo@manca.eu>
2015-01-12 21:11:10 +0100

    Email: updated text version and converted html to inline styles

--- mail/booking/customer_booking_html.php
+++ mail/booking/customer_booking_html.php
@@ -10,65 +10,70 @@ use yii\helpers\Url;
 /* @var $entryFee integer */
 /* @var $timeslots Timeslot[] */
 
+$style_table = 'border-spacing: 0;border-collapse: collapse!important;background-color: transparent;width: 100%;max-width: 100%;margin-bottom: 20px;';
+$style_td_even = 'padding: 8px;line-height: 1.42857143;vertical-align: top;border-top: 1px solid #ddd;background-color: #fff!important;';
+$style_td_odd = 'padding: 8px;line-height: 1.42857143;vertical-align: top;border-top: 1px solid #ddd;background-color: #fafafa!important;';
+$style_btn = 'background-color: #337ab7;color: #fff;text-decoration: none;display: inline-block;padding: 6px 12px;margin-bottom: 0;font-size: 14px;font-weight: 400;line-height: 1.42857143;text-align: center;white-space: nowrap;vertical-align: middle;-ms-touch-action: manipulation;touch-action: manipulation;cursor: pointer;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;background-image: none;border: 1px solid transparent;border-radius: 4px;border-color: #2e6da4;';
+
 ?>
 
 <?= Html::tag('p',
     Yii::t('app', 'Hello, {0} <br> we have received your request. Below you can find a summary of your booking:',
-        $booking->name)) ?>
+        $booking->name), ['style' => 'margin: 0 0 10px;']) ?>
 
-<table class="table table-striped">
+<table class="table table-striped" style="<?= $style_table ?>">
     <tbody>
     <tr>
-        <?= Html::tag('td', Yii::t('app', 'Name')) ?>
-        <?= Html::tag('td', $booking->name . ' ' . $booking->surname) ?>
+        <?= Html::tag('td', Yii::t('app', 'Name'), ['style' => $style_td_odd]) ?>
+        <?= Html::tag('td', $booking->name . ' ' . $booking->surname, ['style' => $style_td_odd]) ?>
     </tr>
     <tr>
         <?= Html::tag('td', Yii::t('app', 'Status')) ?>
         <?= Html::tag('td', $booking->status) ?>
     </tr>
     <tr>
-        <?= Html::tag('td', Yii::t('app', 'Secret key')) ?>
-        <?= Html::tag('td', $booking->token) ?>
+        <?= Html::tag('td', Yii::t('app', 'Secret key'), ['style' => $style_td_odd]) ?>
+        <?= Html::tag('td', $booking->token, ['style' => $style_td_odd]) ?>
     </tr>
     <tr>
         <?= Html::tag('td', Yii::t('app', 'Flight cost')) ?>
-        <?= Html::tag('td', $totalSimulationCost) ?>
+        <?= Html::tag('td', $totalSimulationCost .' kr') ?>
     </tr>
     <tr>
-        <?= Html::tag('td', Yii::t('app', 'Entry fee')) ?>
-        <?= Html::tag('td', $entryFee) ?>
+        <?= Html::tag('td', Yii::t('app', 'Entry fee'), ['style' => $style_td_odd]) ?>
+        <?= Html::tag('td', $entryFee .' kr', ['style' => $style_td_odd]) ?>
     </tr>
     <tr>
         <?= Html::tag('td', Yii::t('app', 'Total cost')) ?>
-        <?= Html::tag('td', $totalSimulationCost + $entryFee) ?>
+        <?= Html::tag('td', $totalSimulationCost + $entryFee .' kr') ?>
     </tr>
     </tbody>
 </table>
 
-<h3>Booked simulations:</h3>
+<h3 style="page-break-after: avoid;font-family: inherit;font-weight: 500;line-height: 1.1;color: inherit;margin-top: 20px;margin-bottom: 10px;font-size: 24px;"><?= Yii::t('app', 'Booked simulations') ?></h3>
 
 <?php
 
 foreach ($timeslots as $timeslot) {
     $simulator = $timeslot->simulator;
     ?>
-    <table class="table table-striped">
+    <table class="table table-striped" style="<?= $style_table ?>">
         <tbody>
         <tr>
-            <?= Html::tag('td', Yii::t('app', 'Simulator')) ?>
-            <?= Html::tag('td', $simulator->name) ?>
+            <?= Html::tag('td', Yii::t('app', 'Simulator'), ['style' => $style_td_odd]) ?>
+            <?= Html::tag('td', $simulator->name, ['style' => $style_td_odd]) ?>
         </tr>
         <tr>
             <?= Html::tag('td', Yii::t('app', 'Start')) ?>
-            <?= Html::tag('td', Yii::t('app', $timeslot->start)) ?>
+            <?= Html::tag('td', $timeslot->start) ?>
         </tr>
         <tr>
-            <?= Html::tag('td', Yii::t('app', 'End')) ?>
-            <?= Html::tag('td', Yii::t('app', $timeslot->end)) ?>
+            <?= Html::tag('td', Yii::t('app', 'End'), ['style' => $style_td_odd]) ?>
+            <?= Html::tag('td', $timeslot->end, ['style' => $style_td_odd]) ?>
         </tr>
         <tr>
             <?= Html::tag('td', Yii::t('app', 'Cost')) ?>
-            <?= Html::tag('td', Yii::t('app', $timeslot->calculateCost())) ?>
+            <?= Html::tag('td', $timeslot->calculateCost() .' kr') ?>
         </tr>
         </tbody>
     </table>
@@ -77,6 +82,6 @@ foreach ($timeslots as $timeslot) {
 <?php
 }
 ?>
-<p>
-    <a class="btn btn-primary" href="<?= Url::to(['view', 'id' => $booking->id, 'token' => $booking->token], true) ?>"><?= Yii::t('app', 'View or cancel') ?></a><?= Yii::t('app', ' your booking on our website') ?>
+<p style="margin: 0 0 10px;">
+    <a class="btn btn-primary" style="<?= $style_btn ?>" href="<?= Url::to(['view', 'id' => $booking->id, 'token' => $booking->token], true) ?>"><?= Yii::t('app', 'View or cancel') ?></a><?= Yii::t('app', ' your booking on our website') ?>
 </p>
--- mail/booking/customer_booking_text.php
+++ mail/booking/customer_booking_text.php
@@ -1,20 +1,52 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Ale
- * Date: 06/01/2015
- * Time: 18:18
- */
 
-echo "Here there are your booking information: \n
-Your Secret key is ". $booking->token . "\n\n
-You have booked the following simulator: \n";
-Yii::info($timeslots);
-foreach($timeslots as $slot){
-    //$simulator = $slot->getSimulator();
+use app\models\Booking;
+use app\models\Timeslot;
+use yii\helpers\Html;
+use yii\helpers\Url;
 
-    echo ""." \n
-    starting from ". $slot->start . "\n
-    ending at " . $slot->end. "\n\n";
+/* @var $booking Booking */
+/* @var $totalSimulationCost integer */
+/* @var $entryFee integer */
+/* @var $timeslots Timeslot[] */
+?>
 
+<?= Yii::t('app', 'Hello, {0} \n we have received your request. Below you can find a summary of your booking:\n\n', $booking->name) ?>
+
+
+        <?= Yii::t('app', 'Name') .': '. $booking->name . ' ' . $booking->surname .'\n' ?>
+
+        <?= Yii::t('app', 'Status') .': '. $booking->status .'\n' ?>
+
+        <?= Yii::t('app', 'Secret key') .': '. $booking->token .'\n' ?>
+
+        <?= Yii::t('app', 'Flight cost') .': '. $totalSimulationCost .' kr\n' ?>
+
+        <?= Yii::t('app', 'Entry fee') .': '. $entryFee .' kr\n' ?>
+
+        <?= Yii::t('app', 'Total cost') .': '. ($totalSimulationCost + $entryFee) .' kr\n' ?>
+
+        \n\n\n
+
+        <?= Yii::t('app', 'Booked simulations') ?>\n\n
+
+<?php
+
+foreach ($timeslots as $timeslot) {
+    $simulator = $timeslot->simulator;
+    ?>
+            <?= Yii::t('app', 'Simulator') .': '. $simulator->name ?>\n
+
+            <?= Yii::t('app', 'Start') .': '. $timeslot->start ?>\n
+
+            <?= Yii::t('app', 'End') .': '. $timeslot->end ?>\n
+
+            <?= Yii::t('app', 'Cost') .': '. $timeslot->calculateCost() .' kr' ?>\n
+
+    \n---------------\n
+<?php
 }
+?>
+\n\n
+    <?= Yii::t('app', 'View or cancel your booking on our website: {0}', Url::to(['view', 'id' => $booking->id, 'token' => $booking->token], true)) ?>
+\n
