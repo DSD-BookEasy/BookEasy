3bf80a459ffdccf716c35f4959a77fab4a889aa8
Paolo Manca <paolo@manca.eu>
2015-01-13 23:41:26 +0100

    Removed 'delete' buttons

--- views/staff/view.php
+++ views/staff/view.php
@@ -12,19 +12,12 @@ $this->title = $model->user_name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Staff'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="time-slot-view">
+<div class="staff-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
         <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
     </p>
 
     <?= DetailView::widget([
